3a5037e2a4935f8cc8b81e56cd642eac7864e1ed
Ngcalisa <156148259+Ngcalisa@users.noreply.github.com>
2024-04-24 09:30:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
