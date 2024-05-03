e3d4332ce3d46e02c5f7db7554aeaacb81f39010
Ngcalisa <156148259+Ngcalisa@users.noreply.github.com>
2024-05-03 14:31:14 +0200

    open sorce control

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function refreshWeather(response) {
   let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formatDate(date);
+     timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
